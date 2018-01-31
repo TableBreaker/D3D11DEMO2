63f6f0722156d6cf65c4f374aab760162149bed0
Flag <karexy@qq.com>
2018-01-31 20:21:07 +0800

    fist push

---
+++ D3D11DEMO2/D3D11DEMO2/Graphicsclass.cpp
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "Graphicsclass.h"
+
+GraphicsClass::GraphicsClass()
+{
+
+}
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+
+}
+
+GraphicsClass::~GraphicsClass()
+{
+
+}
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	return true;
+}
+
+void GraphicsClass::Shutdown()
+{
+	return;
+}
+
+bool GraphicsClass::Frame()
+{
+	return true;
+}
+
+bool GraphicsClass::Render()
+{
+	return true;
+}
---
+++ D3D11DEMO2/D3D11DEMO2/Modelclass.cpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: modelclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "modelclass.h"
+
+ModelClass::ModelClass()
+{
+	m_vertexBuffer = NULL;
+	m_indexBuffer = NULL;
+}
+
+ModelClass::ModelClass(const ModelClass& other)
+{
+}
+
+ModelClass::~ModelClass()
+{
+}
+
+bool ModelClass::Initialize(ID3D11Device* device)
+{
+	bool result;
+
+	// Initialize the vertex and index buffer that hold the geometry for the model
+	result = 
+}
